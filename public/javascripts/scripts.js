dcca6ff05078a9a4e983ef6d968d2eb86230c64b
Brian Alves Moreira <brian.moreira@myfreecomm.com.br>
2020-03-09 17:28:45 -0300

    Ajustes gerais

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,21 +1,21 @@
 $(document).ready(function(){
 	$('.slider-home').slick({
 		autoplay: true,
-		arrows: true,
 		dots: true
 	});
 
 	$('.slider-depoimentos').slick({
 		autoplay: true,
-		arrows: true,
 		slidesToShow: 3,
 		slidesToScroll: 3,
-		responsive: [{
-	    breakpoint: 480,
-	    settings: {
-	      slidesToShow: 1,
-	      slidesToScroll: 1
-	    }
-	  }]
+		responsive: [
+			{
+		    breakpoint: 480,
+		    settings: {
+		      slidesToShow: 1,
+		      slidesToScroll: 1
+		    }
+		  }
+	  ]
 	});
 });
