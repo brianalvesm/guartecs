40b7ad15ef794851874965bace4d0f3e4df72143
Brian Alves Moreira <brian.moreira@myfreecomm.com.br>
2020-03-09 02:56:57 -0300

    Estiliza outras sections na home

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -4,4 +4,18 @@ $(document).ready(function(){
 		arrows: true,
 		dots: true
 	});
+
+	$('.slider-depoimentos').slick({
+		autoplay: true,
+		arrows: true,
+		slidesToShow: 3,
+		slidesToScroll: 3,
+		responsive: [{
+	    breakpoint: 480,
+	    settings: {
+	      slidesToShow: 1,
+	      slidesToScroll: 1
+	    }
+	  }]
+	});
 });
