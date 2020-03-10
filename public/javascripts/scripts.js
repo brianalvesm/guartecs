0761a2d80dd6f02b56728120c1bd44c24e27e28d
Brian Alves Moreira <brian.moreira@myfreecomm.com.br>
2020-03-10 13:28:11 -0300

    Ajustes no mobile da home

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -10,4 +10,20 @@ $(document).ready(function(){
 	  return false;
 	});
 
+	var removeFloat = function() {
+		if ($(window).width() < 640) {
+			$('.remove-float-left-mobile').removeClass('float-left');
+			$('.remove-float-right-mobile').removeClass('float-right');
+		} else {
+			$('.remove-float-left-mobile').addClass('float-left');
+			$('.remove-float-right-mobile').addClass('float-right');
+		}
+	};
+
+	$(window).resize(function() {
+	  removeFloat();
+	});
+
+	removeFloat();
+
 });
