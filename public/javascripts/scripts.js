311de2a94525c30a8c68687747e56028abbbe649
Brian Alves Moreira <brian.moreira@myfreecomm.com.br>
2020-03-10 01:45:47 -0300

    Substitui carousel slick pelo orbit

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,21 +1,3 @@
 $(document).ready(function(){
-	$('.slider-home').slick({
-		autoplay: true,
-		dots: true
-	});
 
-	$('.slider-depoimentos').slick({
-		autoplay: true,
-		slidesToShow: 3,
-		slidesToScroll: 3,
-		responsive: [
-			{
-		    breakpoint: 480,
-		    settings: {
-		      slidesToShow: 1,
-		      slidesToScroll: 1
-		    }
-		  }
-	  ]
-	});
 });
